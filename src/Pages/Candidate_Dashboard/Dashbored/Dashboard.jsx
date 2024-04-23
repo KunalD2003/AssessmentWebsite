fbc6c6c9d955b6ea45b2ffe92db504f9c431905c
P <139226197+PriyankaParihar412@users.noreply.github.com>
2024-04-23 21:03:59 +0530

    first edit

--- src/Pages/Candidate_Dashboard/Dashbored/Dashboard.jsx
+++ src/Pages/Candidate_Dashboard/Dashbored/Dashboard.jsx
@@ -4,33 +4,40 @@ import Hero_section from './Hero_section';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 function Dashboard() {
-  return (<>
-    <div className='col-12  ' >
-        <div className='container'>
-        <div className='row justify-content-center align-items-center'>
-            <div className='col-md-2 px-0' style={{ overflowY: 'auto' }}>
-                <Sidebar></Sidebar>
-            </div>
-            <div className='col-10 px-0'>
-                <div className='col-12'>
-                    <div className='row '>
+   
+
+  return (
+    <div className="col-12">
+    <div className='container-fluid'>
+        <div className='col-12 '>
+            <div className='row justify-content-center'>
+                <div className='col-2 px-0  '>
+                 <Sidebar></Sidebar>
+                </div>
+                <div className='col-10 px-0 '>
+                
+                    <div className='row justify-content-center align-items-center'>
                         <div className='col-12'>
-                            <Navbar />
+                            <Navbar/>
                         </div>
                         <div className='col-12'>
                            <Hero_section></Hero_section>
                         </div>
-                        <div className='col-12'>
-                            <Footer></Footer>
+                        <div className='col-12 '>
+                            <Footer/>
                         </div>
                     </div>
-                    </div>
                 </div>
+                    
+                
             </div>
         </div>
     </div>
-    </>
+  </div>
   )
 }
+
+
+ 
 
 export default Dashboard
--- src/Pages/Candidate_Dashboard/Dashbored/Footer.jsx
+++ src/Pages/Candidate_Dashboard/Dashbored/Footer.jsx
@@ -1,36 +1,35 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faYoutube, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import './Footer.css';
+import 'bootstrap/dist/css/bootstrap.css'; // Import Bootstrap CSS
 const Footer = () => {
   return (
-    <footer className="container d-flex footer">
-      {/* Company Information */}
-      <div className="col">
-        <a href="https://www.companywebsite.com">CompanyName</a>
-      </div>
-
-      {/* Contact Information */}
-      <div className="col">
-        <p>Phone: +1-123-456-7890</p>
-        <p>Email: contact@companyemail.com</p>
-      </div>
-
-      {/* Social Media Links */}
-      <div className="col">
-        <p>Connect with us:</p>
-        <a href="https://www.youtube.com/company" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faYoutube} />
-        </a>
-        <a href="https://www.facebook.com/company" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faFacebook} />
-        </a>
-        <a href="https://www.instagram.com/company" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
+    <footer className="bg-dark text-white py-4">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-4">
+            <h5>AveryBit Solutions Private Limited</h5>
+            <p>36, Bicholi Hapsi Road,
+            near Phoenix Hospital,
+            Shukh Shanti Nagar, Indore</p>
+          </div>
+          <div className="col-12 col-md-4">
+            <h5>Contact</h5>
+            <ul className="list-unstyled">
+              <li>Email: kunaldeshmukh123@gmail.com</li>
+              <li>Phone: (123) 456-7890</li>
+            </ul>
+          </div>
+          {/* Social Media Links */}
+          <div className="col-12 col-md-4">
+            <h5>Follow Us</h5>
+            <ul className="list-unstyled d-flex gap-3">
+              <li><a href="https://facebook.com" className="text-white"><i className="bx bxl-facebook bg-color-primary"></i></a></li>
+              <li><a href="https://instagram.com" className="text-white"><i className="bx bxl-instagram"></i></a></li>
+              <li><a href="https://youtube.com" className="text-white"><i className="bx bxl-youtube"></i></a></li>
+            </ul>
+          </div>
+        </div>
       </div>
     </footer>
   );
 };
-
 export default Footer;
