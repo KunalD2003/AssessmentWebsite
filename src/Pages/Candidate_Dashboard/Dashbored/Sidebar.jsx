b530957675a7e238cc6193645d7634cb68536fdc
krishna <prashu9098@gmail.com>
2024-05-16 12:40:20 +0530

    small changes

--- src/Pages/Candidate_Dashboard/Dashbored/Sidebar.jsx
+++ src/Pages/Candidate_Dashboard/Dashbored/Sidebar.jsx
@@ -5,19 +5,19 @@ import { NavLink } from 'react-router-dom';
 import logo from '../../../Components/Authentication/images/AveryBit-Full-114.webp'
 const Sidebar = () => {
     return (
-        <div className='sidebar'>
+        <div className='sidebar' style={{position:"fixed",width:'18%'}}>
             <div className='sidebar-logo-div'>
                 <img src={logo} alt="" className='sidebar-logo' />
             </div>
             <div className='sidebar-naivgation'>
                 <div>
-                    <h2><NavLink to="/userid/assessments">Dashboard</NavLink></h2>
+                    <h4><NavLink to="/userid/assessments">Dashboard</NavLink></h4>
                 </div>
                 <div>
-                    <h2><NavLink to="/userid/archivedexams">Archived Exams</NavLink></h2>
+                    <h4><NavLink to="/userid/archivedexams">Archived Exams</NavLink></h4>
                 </div>
                 <div>
-                    <h2><NavLink to="/userid/support">Support</NavLink></h2>
+                    <h4><NavLink to="/userid/support">Support</NavLink></h4>
                 </div>
             </div>
         </div>
