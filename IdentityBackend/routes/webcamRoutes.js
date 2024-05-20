b52a5a816bcbaf3aab011243378761c03d9c6e60
P <139226197+PriyankaParihar412@users.noreply.github.com>
2024-05-20 11:29:58 +0530

    Correct folder structure of identity scanning backend

--- IdentityBackend/routes/webcamRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { addUserIdentity } = require('../controllers/userIdentityController'); // Import the controller function
-const router = express.Router();
-
-// Define the POST endpoint for storing captured images
-router.post('/saveImage', addUserIdentity); // Use the controller function to store the image
-
-module.exports = router; // Export the router
