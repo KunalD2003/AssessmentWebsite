51b189732d1b0fec7d8bab73c94d1758875ca90e
Shivam vaidhya <110667876+ShivamV11@users.noreply.github.com>
2024-05-20 15:17:39 +0530

    update 2 files

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0', // Bind to 0.0.0.0 to accept connections from any network interface
-    port: process.env.PORT || 3000, // Use the PORT environment variable or default to 3000
-    proxy: {
-      '/api/assessments': {
-        target: 'https://assessmentwebsite-6.onrender.com',
-        changeOrigin: true, // Needed for virtual hosted sites
-        secure: false, // If the target site uses self-signed SSL certificates
-      },
+  server:{
+    proxy:{
+    '/api/assessments': 'https://assessmentwebsite-6.onrender.com',
     },
   },
   plugins: [react()],
