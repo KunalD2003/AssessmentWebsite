30b3e4bff8be02f13182f49e49c0e547ae2ad738
Shivam vaidhya <110667876+ShivamV11@users.noreply.github.com>
2024-05-20 14:32:31 +0530

    Update vite.config.js
    config updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-    '/api/assessments': 'http://localhost:3000',
+    '/api/assessments': 'https://assessmentwebsite-6.onrender.com',
     },
   },
   plugins: [react()],
