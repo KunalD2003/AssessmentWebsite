5c80cf32e3bdf29cc5f4f1b4da2f1970f9c00cd1
Shivam vaidhya <110667876+ShivamV11@users.noreply.github.com>
2024-05-20 18:10:13 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-    '/api/assessments': 'https://assessmentwebsite-6.onrender.com',
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5143,
+    proxy: {
+      '/api/assessments': 'https://assessmentwebsite-6.onrender.com',
     },
   },
   plugins: [react()],
