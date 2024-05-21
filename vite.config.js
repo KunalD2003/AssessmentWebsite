aad9e7cbc10fb4aa4e3380de155d565e4ea3dba3
Shivam vaidhya <110667876+ShivamV11@users.noreply.github.com>
2024-05-21 17:06:00 +0530

    changes from chatGPT

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     server:{
       host: '0.0.0.0',
-      port: process.env.PORT || 3000,
+      port: process.env.PORT || 5143,
+
       proxy:{
       '/api/mcqquestions': 'https://assessmentwebsite-4-3u7s.onrender.com',
       '/api/codingProblems': 'https://assessmentwebsite-6.onrender.com',
@@ -18,6 +19,10 @@ export default defineConfig({
       '/compilex': 'https://assessmentwebsite-6.onrender.com',
       '/contacts': 'https://assessmentwebsite-4-3u7s.onrender.com/contacts'
       },
+      preview: {
+        host: '0.0.0.0',
+        port: process.env.PORT || 5143,
+      }
     },
   plugins: [react()],
 });
